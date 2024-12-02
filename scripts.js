cc785e8f4b2363255c8514e03712cab65a766c18
Janaínacosta <janainabenicio11@gmail.com>
2024-12-02 08:01:10 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,14 +1,14 @@
 // Script de validação do formulário
 document.addEventListener('DOMContentLoaded', function () {
-    // Seleciona o formulário
+
     const form = document.querySelector('#contactForm');
     
-    // Adiciona um evento de submit
+   
     form.addEventListener('submit', function (event) {
         if (!form.checkValidity()) {
-            event.preventDefault(); // Impede o envio do formulário
-            event.stopPropagation(); // Impede a propagação do evento
+            event.preventDefault(); 
+            event.stopPropagation(); 
         }
-        form.classList.add('was-validated'); // Adiciona a classe para ativar as mensagens
+        form.classList.add('was-validated'); 
     }, false);
 });
